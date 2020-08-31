4e3f7b5630b459e708b5b0f3da4d979fe5f47356
ivishal <vishalinfant2@gmail.com>
2020-08-31 20:34:31 +0530

    version test

--- lib/test_learn_package.dart
+++ lib/test_learn_package.dart
@@ -6,9 +6,11 @@ class TestLearnPackage {
   static Future showCustomAlertBox({
     @required BuildContext context,
     @required Widget willDisplayWidget,
+    @required Widget smallText,
   }) {
     assert(context != null, "context is null!!");
     assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    assert(smallText!= null, "smallText is null!!");
     return showDialog(
         context: context,
         builder: (context) {
@@ -20,6 +22,7 @@ class TestLearnPackage {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 willDisplayWidget,
+                smallText,
                 MaterialButton(
                   color: Colors.white30,
                   child: Text('close alert'),
