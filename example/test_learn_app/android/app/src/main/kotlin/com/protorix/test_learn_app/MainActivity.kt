52c7e7e06917d204720b01aa903855b19951063f
iVishal <ivishal@iVishals-MacBook-Air.local>
2020-08-31 20:01:22 +0530

    Initial commit

---
+++ example/test_learn_app/android/app/src/main/kotlin/com/protorix/test_learn_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.protorix.test_learn_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/test_learn_package.dart
@@ -0,0 +1,7 @@
+library test_learn_package;
+
+/// A Calculator.
+class TestLearnPackage {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
